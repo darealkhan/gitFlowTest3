10a4b85681de9cd4139924268fc05b3a9853a438
Ayxan Səfərli <aykhanseferli@gmail.com>
2022-06-23 12:01:38 +0400

    Initial Commit

---
+++ gitFlowTest3/gitFlowTest3App.swift
@@ -0,0 +1,17 @@
+//
+//  gitFlowTest3App.swift
+//  gitFlowTest3
+//
+//  Created by Ayxan Seferli on 23.06.22.
+//
+
+import SwiftUI
+
+@main
+struct gitFlowTest3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
