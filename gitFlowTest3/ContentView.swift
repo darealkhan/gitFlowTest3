16f8c5125678fbe6951fa608023aef0bcca313dc
Ayxan Səfərli <aykhanseferli@gmail.com>
2022-06-23 12:03:28 +0400

    New Button Feature

--- gitFlowTest3/ContentView.swift
+++ gitFlowTest3/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            
+            Button {
+                print("Salam qaqa")
+            } label: {
+                Text("Click me")
+            }
+
+        }
     }
 }
 
